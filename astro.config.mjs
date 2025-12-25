07b1e03da014b21014cf04a35e35ccbe2836b89f
HarishChoure <harishchoureoutlook@gmail.com>
2025-12-25 15:16:34 +0530

    Fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/quick-launch/',
+  base: '/',
   integrations: [tailwind()],
   output: 'static',
 });
